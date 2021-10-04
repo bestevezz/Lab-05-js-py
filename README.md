fc9e847760b99d52f0f067cab432e8416087fa4c
unknown <bryan.estevez@mail.citytech.cuny.edu>
2021-10-04 16:11:42 -0400

    finished greetings in both PY and JS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab-05-js-py
 Lab for week 5 of MTEC 1003
+
+This is our first lab for javascript and python
---
+++ greetings.py
@@ -0,0 +1,8 @@
+username = input("Speak your name foul beast")
+    #username is a symbol represtenting a placeholder in memory
+    #input is a function that collects user input
+    #the output of input function is assigned to the variable username
+print("You are a strange one " + str(username) + "...")
+    #print is a print function in python
+    #str is a function in python that converts its input into a string
+    # + os a symbol for string concatenation!
