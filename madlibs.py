b8354fbeb48d245065a24b17fc93cff787a67965
unknown <bryan.estevez@mail.citytech.cuny.edu>
2021-10-04 17:00:59 -0400

    ADDING MADLIBS

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+noun = input("Choose a Noun: ")
+adjective = input("Now an ADJECTIVE dude: ")
+adverb = input("AN ADVERB IF U KNOW WHAT THAT IS: ")
+verb = input("And now a VERB: ")
+
+print("On a tropical " + str(noun) + ",")
+print("underneath a " + str(adjective) + " lava moon.")
+print("Hangin' " + str(adverb) + " with the hulu dancers,")
+print(str(verb) + " questions cause' they all got the answers.")
